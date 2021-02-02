2b5ced9117d9e028dd327bc905246b5dedbd3e5c
Dan Kreibich <dan@lh-innovationhub.com>
2021-02-02 22:15:25 +0100

    inclusion of fiege values

--- csv_extractor.rb
+++ csv_extractor.rb
@@ -3,12 +3,13 @@ require 'csv'
 require_relative 'api_calls'
 
 # change to respective CSV
-file = 'spherea2020.csv'
+file = 'fiege_2019.csv'
+file_base_name = file.split('.').first
 
-def run_program(file)
+def run_program(file, file_base_name)
   input_array = get_data(file)
   output_array = get_carbon_data(input_array)
-  save_to_csv(output_array)
+  save_to_csv(output_array, file_base_name)
 end
 
 def get_data(file)
@@ -19,6 +20,7 @@ def get_data(file)
     origin = locations[0]
     destination = locations[1]
     booking_class = values[1]
+    number_of_flights = values[2].to_i
 
     if booking_class == 'Eco'
       booking_class = 'economy'
@@ -33,7 +35,8 @@ def get_data(file)
     flight_hash = { origin: origin,
                     destination: destination,
                     booking_class: booking_class,
-                    number_of_travelers: 1 }
+                    number_of_travelers: 1,
+                    number_of_flights: number_of_flights }
     input_array << flight_hash
   end
   input_array
@@ -42,16 +45,16 @@ end
 def get_carbon_data(input_array)
   output_array = []
   input_array.each do |flight|
-    output_array << [flight[:origin], flight[:destination], flight[:booking_class], request_flight_emissions_csv(flight)]
+    output_array << [flight[:origin], flight[:destination], flight[:booking_class], request_flight_emissions_csv(flight) * flight[:number_of_flights]]
   end
   output_array
 end
 
-def save_to_csv(output_array)
-  CSV.open('new_file.csv', 'wb') do |csv|
+def save_to_csv(output_array, file__base_name)
+  CSV.open(file__base_name + "_results.csv", 'wb') do |csv|
     csv << ['origin', 'destination', 'class', 'CO2 in grams']
     output_array.each { |x| csv << x }
   end
 end
 
-run_program(file)
+run_program(file, file_base_name)
